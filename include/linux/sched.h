840c3065c27540ae23f9bfe37838f08a70d93bbc
nullptr-t-oss <127822226+nullptr-t-oss@users.noreply.github.com>
2026-05-08 16:59:55 +0800

    [PATCH] ABI: use ABI padding for SYSVIPC
    This commit allows us to enable CONFIG_SYSVIPC in defconfig without breaking kernel ABI.
    
    Signed-off-by: nullptr-t-oss <127822226+nullptr-t-oss@users.noreply.github.com>
    
    Signed-off-by: ianchb <i@4t.pw>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1088,8 +1088,8 @@ struct task_struct {
 	struct nameidata		*nameidata;
 
 #ifdef CONFIG_SYSVIPC
-	struct sysv_sem			sysvsem;
-	struct sysv_shm			sysvshm;
+	// struct sysv_sem			sysvsem;
+	// struct sysv_shm			sysvshm;
 #endif
 #ifdef CONFIG_DETECT_HUNG_TASK
 	/* hung task detection */
@@ -1474,9 +1474,14 @@ struct task_struct {
 
 	ANDROID_VENDOR_DATA_ARRAY(1, 3);
 
+#ifdef CONFIG_SYSVIPC
+	ANDROID_KABI_USE(1, struct sysv_sem sysvsem);
+	_ANDROID_KABI_REPLACE(ANDROID_KABI_RESERVE(2); ANDROID_KABI_RESERVE(3), struct sysv_shm sysvshm);
+#else
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
 	ANDROID_KABI_RESERVE(3);
+#endif
 	ANDROID_KABI_RESERVE(4);
 	ANDROID_KABI_RESERVE(5);
 	ANDROID_KABI_RESERVE(6);
